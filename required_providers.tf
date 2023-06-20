b2960f18c07a555132765211312e84bdfb8dfb9d
shashi-web <raoshashidhar.3@gmail.com>
2023-06-20 12:23:11 -0400

    state backend

--- required_providers.tf
+++ required_providers.tf
@@ -1,4 +1,19 @@
 terraform {
+  # backend "remote" {
+  #   hostname = "app.terraform.io"
+  #   organization = "shashi-terraform"
+
+  #   workspaces {
+  #     name = "terraform-bootcamps"
+  #   }
+  # }
+  cloud {
+    organization = "shashi-terraform"
+
+    workspaces {
+      name = "terraform-bootcamps"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
